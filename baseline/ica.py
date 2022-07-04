05268584707a74c38c7d13e22865321bc06d15c6
lyh <412929473@qq.com>
2022-07-04 22:51:44 +0800

    modify ica add masker

--- baseline/ica.py
+++ baseline/ica.py
@@ -1,31 +1,40 @@
-from nilearn.input_data import NiftiMasker
 from nilearn.decomposition import CanICA
 
+from baseline.masker import Masker
 
-class ICA:
-    def __init__(self, mask_path, data_path,
-                 n_components, random_state):
-        self.mask_path = mask_path
-        self.data_path = data_path
-        self.n_components = n_components
-        self.random_state = random_state
-        self.Ica_3d = None
+class ICA(Masker):
+	def __init__(self, mask_path, data_path,
+				 n_components, random_state):
+		super(ICA, self).__init__(mask_path=mask_path)
+		self.mask_path = mask_path
+		self.data_path = data_path
+		self.n_components = n_components
+		self.random_state = random_state
+		self.ica = CanICA(mask=self.mask_path,
+						n_components=self.n_components,
+						memory="nilearn_cache", memory_level=2,
+						random_state=self.random_state)
 
-    def fit(self):
-        canica = CanICA(mask=self.mask_path,
-                        n_components=self.n_components,
-                        memory="nilearn_cache", memory_level=2,
-                        random_state=self.random_state)
-        canica.fit(self.data_path)
+	def fit(self, data_path=None):
+		if data_path is not None:
+			self.ica.fit(data_path)
+		else:
+			self.ica.fit(self.data_path)
+		return self
 
-        self.Ica_3d = canica.components_img_
+	def get_3d(self):
+		return self.ica.components_img_
 
-        return self
+	def get_2d(self):
+		return self.transform(self.get_3d().get_fdata())
 
-    def get_2d(self):
-        masker = NiftiMasker(mask_img=self.mask_path,
-                             standardize=False)
-        masker.fit()
-        Ica_2d = masker.transform(self.Ica_3d)
+	def setNcomponents(self, n_components):
+		self.n_components = n_components
+		self.ica = CanICA(mask=self.mask_path,
+						  n_components=self.n_components,
+						  memory="nilearn_cache", memory_level=2,
+						  random_state=self.random_state)
 
-        return Ica_2d
+	def fit_transform(self, data_path):
+		self.fit(data_path)
+		return self.get_2d()
