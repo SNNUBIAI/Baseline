beb40af1c7ddf2882fe2688a418fdab5ca97502a
lyh <412929473@qq.com>
2022-09-21 20:29:02 +0800

    pip install .

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+  name="baseline",
+  version="0.0.1",
+  author="Yiheng Liu and Enjie Ge",
+  description="A package to help compare functional brain network's performance.",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/SNNUBIAI/Baseline",
+  packages=setuptools.find_packages(),
+  classifiers=[
+  "Programming Language :: Python :: 3",
+  "License :: OSI Approved :: MIT License",
+  "Operating System :: OS Independent",
+  ],
+)
