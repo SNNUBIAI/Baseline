82e22d7a29ea81be457c6c705cb08af6f450dec1
lyh <412929473@qq.com>
2022-09-29 10:35:55 +0800

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setuptools.setup(
   name="baseline",
-  version="0.0.1",
+  version="0.0.2",
   author="Yiheng Liu and Enjie Ge",
   description="A package to help compare functional brain network's performance.",
   long_description=long_description,
