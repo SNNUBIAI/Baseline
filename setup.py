db08edc2518b62833cdd0cd517589c6dfdc7b687
lyh <412929473@qq.com>
2022-09-29 22:12:36 +0800

    add package_data

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setuptools.setup(
   name="baseline",
-  version="0.0.2",
+  version="0.0.3",
   author="Yiheng Liu and Enjie Ge",
   description="A package to help compare functional brain network's performance.",
   long_description=long_description,
   long_description_content_type="text/markdown",
   url="https://github.com/SNNUBIAI/Baseline",
   packages=setuptools.find_packages(),
+  package_data={'baseline.templates.data': ["*.npy", "*/*.mat"]},
   classifiers=[
   "Programming Language :: Python :: 3",
   "License :: OSI Approved :: MIT License",
