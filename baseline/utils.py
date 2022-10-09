1523740309f6b076491b0e30b817d22383bd83b8
lyh <412929473@qq.com>
2022-10-09 21:25:24 +0800

    IoU evaluate

--- baseline/utils.py
+++ baseline/utils.py
@@ -1,7 +1,18 @@
 import numpy as np
-from nilearn.plotting import plot_prob_atlas
 from nilearn.image import iter_img
 from nilearn.plotting import plot_stat_map, show
+
+def IoU(n1, n2):
+	"""
+	:param n1: 1*N
+	:param n2: 1*N
+	:return: IoU
+	"""
+	intersect = np.logical_and(n1, n2)
+	union = np.logical_or(n1, n2)
+	I = np.count_nonzero(intersect)
+	U = np.count_nonzero(union)
+	return I / U
 
 def flip(row):
 	if np.sum(row > 0) < np.sum(row < 0):
@@ -32,7 +43,15 @@ def plot_net(components_img,
 			show()
 	else:
 		for i, cur_img in enumerate(iter_img(components_img)):
-			print(i + 1)
 			plot_stat_map(cur_img, display_mode="z",
 						  cut_coords=cut_coords, colorbar=colorbar, annotate=annotate)
 			show()
+
+def evaluate_iou(img2d, template):
+	iou = np.zeros((img2d.shape[0], template.shape[0]))
+	for i in range(img2d.shape[0]):
+		for j in range(template.shape[0]):
+			iou[i, j] = IoU(template[j:j + 1, :], img2d[i:i + 1, :])
+	print(iou.max(axis=0))
+	print(iou.argmax(axis=0))
+	return iou
